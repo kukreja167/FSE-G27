2447471d1357cf4486d0bdd665c179edf2bdde78
kukreja167 <kavyakukreja167@gmail.com>
2026-02-26 12:22:51 +0530

    changes

--- lec_12-react/Conditional-rendering/src/App.jsx
+++ lec_12-react/Conditional-rendering/src/App.jsx
@@ -10,23 +10,24 @@ function App() {
 
   return (
     <div>
-     <h1>Number is: {num}</h1>
+<h1>WIN - 7</h1>
+     <h2>Number is: {num}</h2>
 <button onClick={check}>Check</button>
-      {num && (num === 7 ? (
-        <>
+      {num !==null && (num === 7 ? (
+        <div>
           <h1>You win</h1>
           <img
-            src="https://www.shutterstock.com/image-vector/you-win-lettering-pop-art-260nw-1606108018.jpg"
+            src="https://media.tenor.com/2amPKT6KY0wAAAAM/chihuahua-chihuahua-laughing.gif"
           />
-        </>
+        </div>
       ) : (
         (
-          <>
+          <div>
             <h1>You lose</h1>
             <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYtkF-RyJp9s_p144I00nfNmW2fjav9cn5UQ&s"
+              src="https://media.tenor.com/pFz1Q12_hXEAAAAM/cat-holding-head-cat.gif"
             />
-          </>
+          </div>
         )
       ))}
     </div>
