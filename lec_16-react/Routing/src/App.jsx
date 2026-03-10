c5406c6d0fd4f0c4cea14f8177536c813a3a7bd0
kukreja167 <kavyakukreja167@gmail.com>
2026-03-10 15:05:23 +0530

    React-router-DOM

--- lec_16-react/Routing/src/App.jsx
+++ lec_16-react/Routing/src/App.jsx
@@ -1,1 +1,59 @@
-import React from 'react'
+import React from 'react';
+//react router dom
+import {BrowserRouter, Route, Routes,Link} from 'react-router-dom';
+ function App(){
+    return (    
+        <div>
+
+
+
+
+            <BrowserRouter>
+            <header>
+    <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
+    </ul>
+</header>
+<Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+</Routes>
+            </BrowserRouter>
+
+        </div>
+    );
+}
+
+
+function Home(){
+    return(
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );  
+}
+function About(){
+        return(
+            <div>
+                <h1>About Page</h1>
+            </div>
+        );  
+    }
+
+ function Contact(){
+        return(
+            <div>
+                <h1>Contact Page</h1>
+            </div>
+        );  
+    }
+
+
+
+
+
+
+export default App;
